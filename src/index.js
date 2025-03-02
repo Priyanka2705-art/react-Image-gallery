29cc95f9d8ac58631284ae4e9ef297b53e00f63f
Arunkumar Kamatchi <arunkumarkamatchi94@gmail.com>
2025-03-03 00:07:48 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./style.css";
+
+import one from "./assets/images/one.jpg"
+import two from "./assets/images/two.jpg"
+import three from "./assets/images/three.jpg"
+import four from "./assets/images/four.jpg"
+import five from "./assets/images/five.jpg"
+import six from "./assets/images/six.jpg"
+import seven from "./assets/images/seven.jpg"
+import eight from "./assets/images/eight.jpg"
+
+
+const root = ReactDom.createRoot(document.getElementById("root"))
+
+function ImageGallery() {
+
+  return (
+    <div class="gallery-container">
+        <div class="box">
+            <img src={one}></img>
+            <p>Natural falls</p>
+        </div>
+    
+        <div class="box">
+        <img src={two}></img>
+            <p>Beautiful Creature</p>
+        </div>
+    
+        <div class="box">
+        <img src={one}></img>
+            <p>Peaceful sight seeing</p>
+        </div>
+    
+        <div class="box">
+        <img src={one}></img>
+            <p>Natural falls</p>
+        </div>
+    
+        <div class="box">
+        <img src={one}></img>
+            <p>Peaceful sight seeing</p>
+        </div>
+    
+        <div class="box">
+        <img src={one}></img>
+            <p>Good ambiance </p>
+        </div>
+    
+        <div class="box">
+        <img src={one}></img>
+            <p>Beautiful Creature</p>
+        </div>
+    
+        <div class="box">
+        <img src={one}></img>
+            <p>Natural falls</p>
+    
+        </div>
+    </div>
+  );
+}
+
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <div>
+    <ImageGallery></ImageGallery>
+  </div>
+)
